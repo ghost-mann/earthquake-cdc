c486657e5200b39b06a269b2d5623eeba395b0a5
ghost-mann <iamaob8@protonmail.com>
2025-07-07 11:56:57 +0300

    feat: get and format time in main.py

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import pandas as pd
 import requests
 from datetime import datetime, timedelta
 
-
+# get current time
 end_time = datetime.now()
 
+# get time one minute ago
+start_time = end_time - timedelta(minutes=1)
 
-url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={NOW-1min ISO8601}&endtime={NOW ISO8601}'
+# format time stamps in ISO-8601
+start_iso = start_time.isoformat()
+end_iso = end_time.isoformat()
+
+
+url = f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_iso}&endtime={end_iso}'
 
 response = requests.get(url)
 
